7d6efb33d9de16699a3c53063a222656bc9fb3a3
weat0212 <weat0212@gmail.com>
2020-11-01 20:24:27 +0800

    backup

---
+++ src/main/java/com/example/spring5webapp/domain/Author.java
@@ -0,0 +1,93 @@
+package com.example.spring5webapp.domain;
+
+import org.springframework.data.web.PageableDefault;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author weat0212@gmail.com
+ * @project spring5webapp
+ * @package com.example.spring5webapp.domain
+ * @date 2020/10/24 下午 04:32
+ */
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author() {
+
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/example/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring5webapp.repositories;
+
+/**
+ * @author weat0212@gmail.com
+ * @project spring5webapp
+ * @package com.example.spring5webapp.repositories
+ * @date 2020/10/25 下午 05:17
+ */
+public interface AuthorRepository extends Crud{
+}
